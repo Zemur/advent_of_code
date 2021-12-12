370827c76b763d41c7bfd8bab3ee9b43d5279d21
Zemur <zemurthelemur@gmail.com>
2021-12-12 03:07:05 -0600

    Day 07

--- 2021/day07/part2.py
+++ 2021/day07/part2.py
@@ -2,17 +2,14 @@ if __name__ == '__main__':
     with open('input', 'r') as f:
         positions = list(map(int, f.read().split(',')))
 
-    min_pos = min(positions)
-    max_pos = max(positions)
-    destinations = range(min_pos, max_pos)
+    destinations = range(min(positions), max(positions))
     all_fuels = []
 
     for dest in destinations:
         total_fuel = []
         for pos in positions:
             fuel_spent = abs(pos - dest)
-            fuel_to_position = ((fuel_spent**2 + fuel_spent) / 2)
-            total_fuel.append(fuel_to_position)
+            total_fuel.append(((fuel_spent**2 + fuel_spent) / 2))
         all_fuels.append(sum(total_fuel))
 
     print(int(min(all_fuels)))
